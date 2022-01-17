423944a431f026f9009639bd94071c9e660ce797
neonfuzz <addie.kingsland@gmail.com>
2022-01-17 12:31:15 -0800

    Moving sphinx setup to pyproject.toml.
    Also major lint of conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,62 +1,33 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
+from datetime import date
 import os
 import sys
+import toml
 
+
+# Path setup.
 sys.path.insert(0, os.path.abspath(".."))
 
 
-# -- Project information -----------------------------------------------------
-project = "palette_generator"
-copyright = "2022, neonfuzz"
-author = "neonfuzz"
-
-# The full version, including alpha/beta/rc tags
-release = "0.1.0"
+# Read info from toml.
+with open("../pyproject.toml", "rt", encoding="utf-8") as infile:
+    config = toml.load(infile)
 
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.napoleon",
-    "sphinxarg.ext",
-    "sphinx_rtd_dark_mode",
-]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+# Project information.
+project = config["tool"]["poetry"]["name"]
+author = config["tool"]["poetry"]["authors"][0]
+copyright = f"{date.today().year}, {author}"
+release = config["tool"]["poetry"]["version"]
 
 
-# -- Options for HTML output -------------------------------------------------
+# General configuration.
+extensions = config["tool"]["sphinx"]["extensions"]
+templates_path = config["tool"]["sphinx"]["templates_path"]
+exclude_patterns = config["tool"]["sphinx"]["exclude_patterns"]
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
 
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {"collapse_navigation": False}
-default_dark_mode = True
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# Options for HTML output.
+html_theme = config["tool"]["sphinx"]["html_theme"]
+html_static_path = config["tool"]["sphinx"]["html_static_path"]
+html_theme_options = config["tool"]["sphinx"]["html_theme_options"]
+default_dark_mode = config["tool"]["sphinx"]["default_dark_mode"]
